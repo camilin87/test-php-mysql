07ce0ae56416cb35d69d6174510251e974022ae0
camilin87 <camilin87@gmail.com>
2016-08-11 23:05:30 -0400

    the save should be a post

--- save.php
+++ save.php
@@ -1,4 +1,8 @@
 <?php
+    if ($_SERVER["REQUEST_METHOD"] != "POST") {
+        die("Invalid request method");
+    }
+
     $servername = $_ENV['MYSQLDB_PORT_3306_TCP_ADDR'];
     $username = "root";
     $password = $_ENV['MYSQLDB_ENV_MYSQL_ROOT_PASSWORD'];
@@ -9,13 +13,16 @@
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    $sql = "INSERT INTO submissions (vendor, ingredient) VALUES ('nike', 'rubber')";
+    $vendor = test_input($_POST["vendor"]);
+    $ingredient = test_input($_POST["ingredient"]);
 
-    if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
-    } else {
+    $sql = "INSERT INTO submissions (vendor, ingredient) VALUES ('".$vendor."', '".$ingredient."')";
+
+    if (!mysqli_query($conn, $sql)) {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 
     mysqli_close($conn);
+
+    echo "New record created successfully";
 ?>
