99ae4ddec5c54cb60dd2dca7506f3d91c9cfed83
camilin87 <camilin87@gmail.com>
2016-08-11 22:55:32 -0400

    save submissions

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-    echo "testing";
+    $servername = $_ENV['MYSQLDB_PORT_3306_TCP_ADDR'];
+    $username = "root";
+    $password = $_ENV['MYSQLDB_ENV_MYSQL_ROOT_PASSWORD'];
+    $dbname = "sampledb";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO submissions (vendor, ingredient) VALUES ('nike', 'rubber')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 ?>
